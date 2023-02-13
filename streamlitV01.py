166f22a92933c53b61febe8858f5cb381bbc6c5f
av3wagner <67894780+av3wagner@users.noreply.github.com>
2023-02-13 14:30:21 +0100

    Update streamlitV01.py

--- streamlitV01.py
+++ streamlitV01.py
@@ -92,7 +92,12 @@ def select_file():
     fileList = []
     extensions = ['pdf', 'docx']
     fileList = listdir(parent_path)
-    onlyfiles = [f for f in fileList if (isfile(join(parent_path, f)) and filename.endswith(extensions))]    
+    
+    #path = "/dataPath/"
+    #onlyTxtFiles = [f for f in listdir(path) if isfile(join(path, f)) and  f.endswith(".txt")]
+    #print onlyTxtFiles
+        
+    onlyfiles = [f for f in fileList if (isfile(join(parent_path, f)) and  f.endswith(".pdf")]    
     option = st.sidebar.selectbox('Pick a dataset', onlyfiles)
     file_location=os.path.join(parent_path, option) # use `file_location` as a parameter to the main script
     if file_location.find('.pdf') > 0:  

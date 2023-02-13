f67d1b44aa9feb153a87ccd276cca54fe6bed5ee
av3wagner <67894780+av3wagner@users.noreply.github.com>
2023-02-13 13:34:43 +0100

    Update streamlitV01.py

--- streamlitV01.py
+++ streamlitV01.py
@@ -99,10 +99,9 @@ def select_file():
     #st.write("data_path: ")
     #st.write(data_path)
     fileList = listdir(parent_path)
-    
-    #for file in FileList:
-    #    if file.split('.')[-1] in extensions:
-    #       print(file) 
+    for file in FileList:
+        if file.split('.')[-1] in extensions:
+           st.write((file) 
     #onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path, f))]
     #option = st.sidebar.selectbox('Pick a dataset', onlyfiles)
     #st.write("option: ")

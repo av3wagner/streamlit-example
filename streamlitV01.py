67964cab49781952dd362b7828bd7dbda8e553bf
av3wagner <67894780+av3wagner@users.noreply.github.com>
2023-02-14 07:24:49 +0100

    Update streamlitV01.py

--- streamlitV01.py
+++ streamlitV01.py
@@ -213,7 +213,7 @@ def select_file_down():
     
 def file_selector(folder_path=path, type=['.docx', '.pdf']):
     filenames = os.listdir(folder_path)
-    selected_filename = st.sitebar.selectbox('Select a file', filenames)
+    selected_filename = st.selectbox('Select a file', filenames)
     return os.path.join(folder_path, selected_filename)
 
 #save_downloadedfile_local(os.path.join(folder_path, selected_filename))
